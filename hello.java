c4809abb77dd1038d8a0060665295a267fe61cec
obrienna <63079907+obrienna@users.noreply.github.com>
2020-04-02 20:06:26 -0400

    Update hello.java

--- hello.java
+++ hello.java
@@ -5,11 +5,11 @@ public class hello {
     public static void main(String [] args) {
 
         // initialize variables
-        String firstName = "Elizabeth";
-        String lastName = "Jones";
+        String firstName = "Nathan";
+        String lastName = "O'Brien";
 	
 		// output welcome message
-        System.out.println("Hello Class!");
+        System.out.println("Hello, my name is " + firstName + " " + lastName);
 
 
     }
